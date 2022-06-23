0c3ece39b8da8b5c1616a55f450b3359ce84d85c
ananthu <ananthu4uuu@gmail.com>
2022-06-24 02:24:55 +0530

    admin page added

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  const product=[{
-    name:"Pegion",
-    price:"5000",
-    Qualities:"Pigeons are gentle, plump, small-billed birds with a skin saddle (cere) between the bill and forehea",
-    image:"https://www.birdnote.org/sites/default/files/rock-pigeon-800-fyn-kynd-cc.jpg"
-  }]
-  res.render('index', {product,admin:false });
+ 
+  res.render('index', {admin:false });
 });
 
 router.get('/login',(req,res,next)=>{
